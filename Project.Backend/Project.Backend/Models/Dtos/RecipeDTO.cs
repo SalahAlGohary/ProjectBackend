0e841f1e93d0aa4f126c80bdfcaebb2c03857406
Salah Mohammed Salah <62995282+SalahAlGohary@users.noreply.github.com>
2024-05-11 16:10:50 +0300

    - fix

--- Project.Backend/Project.Backend/Models/Dtos/RecipeDTO.cs
+++ Project.Backend/Project.Backend/Models/Dtos/RecipeDTO.cs
@@ -6,14 +6,14 @@
         public string Name { get; set; }
         public string? CoverUrl { get; set; }
         public string? Description { get; set; }
-        public decimal Calories { get; set; }
+        //public decimal Calories { get; set; }
         public int NumberOfLikes { get; set; }
         public int CookingTime { get; set; }
         public int PrepTime { get; set; }
         public int Serves { get; set; }
         public int Ratings { get; set; }
         public string? SkillLevel { get; set; }
-        public string? PostDates { get; set; }
+        //public string? PostDates { get; set; }
         public List<string>? NutritionInfos { get; set; }
         public List<string>? Ingredients { get; set; }
         public List<string>? Keywords { get; set; }
